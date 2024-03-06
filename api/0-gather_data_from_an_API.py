02bec9f43325bee15ff39484b5c919933833f705
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-06 14:22:08 -0500

    Receive an employee ID as an argument.

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,37 +1,45 @@
 #!/usr/bin/python3
-"""This script collects information about an employee's task list process using an APIREST."""
+"""
+This script collects information about an employee's task list process using an APIREST.
+"""
+
 import requests
 from sys import argv
 
 
-if __name__ == '__main__':
-    if len(argv) > 1:
-        employee_id = argv[1]
-        """Replace 'BASE_URL' with the actual URL of the API to be queried"""
-        BASE_URL = "https://jsonplaceholder.typicode.com"
+def main(employee_id):
+    """
+    API base URL.
+    """
+    base_url = "https://jsonplaceholder.typicode.com"
 
-        """Obtain employee information"""
-        user_url = f"{BASE_URL}/users/{employee_id}"
-        user_response = requests.get(user_url)
-        employee_name = user_response.json().get('name')
+    # Obtain employee data
+    user_url = f"{base_url}/users/{employee_id}"
+    user_res = requests.get(user_url)
+    user_data = user_res.json()
 
-        """Obtain all tasks associated with the employee"""
-        todos_url = f"{BASE_URL}/todos?userId={employee_id}"
-        todos_response = requests.get(todos_url)
-        todos = todos_response.json()
+    # Obtain employee tasks
+    all_url = f"{base_url}/all?userId={employee_id}"
+    all_res = requests.get(all_url)
+    all_data = all_res.json()
 
-        """Calculate the number of tasks completed and totals"""
-        total_tasks = len(todos)
-        done_tasks = sum(task['completed'] for task in todos)
+    # Calculate progress
+    all_tasks = len(all_data)
+    completed_tasks = len([task for task in all_data if task['completed']])
+    employee_name = user_data['name']
 
-        """Print progress information"""
-        message = (
-            f"Employee {employee_name} is done with tasks"
-            f"({done_tasks}/{total_tasks}):"
-        )
-        print(message)
+    #Print progress information
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{all_tasks}):")
+    for task in all_data:
+        if task['completed']:
+            print(f"\t {task['title']}")
 
-        """Print the titles of completed tasks"""
-        for task in todos:
-            if task['completed']:
-                print(f"\t {task['title']}")
+if __name__=='__main__':
+    if len(argv) == 2:
+        try:
+            employee_id = int(argv[1])
+            main(employee_id)
+        except ValueError:
+            print("Please provide an integer for the employee ID.")
+    else:
+        print("Usage: python3 script.py <employee_id>")
