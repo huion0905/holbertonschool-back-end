785b4794e5ffec25cc591f6c3b4ce3f6a0ac071a
Valery Armas <133952148+huion0905@users.noreply.github.com>
2024-03-07 01:41:40 -0500

    create files

--- api/2-export_to_JSON.py
+++ api/2-export_to_JSON.py
@@ -1,43 +1,57 @@
 #!/usr/bin/python3
 """
-Script to export data into the CSV format.
+Script that retrieves and displays the progress of an employee's TODO list
+using the JSONPlaceholder API.
 """
 
+import json
 import requests
-import csv
-from sys import argv
+import sys
 
 
-def main(employee_id):
-    """Main function to gather data from the API and save it to a CSV file"""
+def get_employee_todo_progress(employee_id):
+    """
+    Fetches and displays the employee's TODO list progress.
+
+    Parameters:
+        employee_id (int): The ID of the employee.
+    """
     base_url = "https://jsonplaceholder.typicode.com"
 
-    # Obtain employee data
-    user_url = f"{base_url}/users/{employee_id}"
-    user_res = requests.get(user_url)
-    user_data = user_res.json()
-    username = user_data['username']
+    # Fetch user details
+    user_response = requests.get(f'{base_url}/users/{employee_id}')
+    user_data = user_response.json()
+    user_id = user_data.get('id')
+    username = user_data.get('username')
 
-    # Obtain employee tasks
-    todos_url = f"{base_url}/todos?userId={employee_id}"
-    todos_res = requests.get(todos_url)
-    todos_data = todos_res.json()
+    # Fetch user's TODO list
+    todos_response = requests.get(f'{base_url}/todos?userId={employee_id}')
+    todos_data = todos_response.json()
 
-    # Create a CSV file and write the data
-    with open(f"{employee_id}.csv", 'w', newline='') as csvfile:
-        task_writer = csv.writer(csvfile, quoting=csv.QUOTE_ALL)
+    # Create JSON data format
+    task_list = []
+    for task in todos_data:
+        task_title = task.get('title')
+        task_status = task.get('completed')
+        task_dict = {"task": task_title,
+                     "completed": task_status,
+                     "username": username}
+        task_list.append(task_dict)
 
-        # Write each task to the CSV
-        for task in todos_data:
-            task_writer.writerow([employee_id, username, task['completed'], task['title']])
+    json_data = {str(user_id): task_list}
+
+    # Write to JSON file
+    json_filename = f'{user_id}.json'
+    with open(json_filename, mode='w') as json_file:
+        json.dump(json_data, json_file)
+
+    print(f'Data exported to {json_filename}')
 
 
 if __name__ == '__main__':
-    if len(argv) == 2:
-        try:
-            employee_id = int(argv[1])
-            main(employee_id)
-        except ValueError:
-            print("Please provide an integer for the employee ID.")
-    else:
-        print("Usage: python3 script.py <employee_id>")
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
